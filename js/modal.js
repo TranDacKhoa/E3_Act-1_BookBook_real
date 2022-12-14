8e353d8f065ba7bfa76e3538ee2dff60b5eeceb5
Tuấn Lê <tuancnttk61@gmail.com>
2022-12-14 16:06:31 +0700

    update

--- js/modal.js
+++ js/modal.js
@@ -1,39 +1,41 @@
 // modal
-export const showModal = (params, data_comment) => {
-   // image
+export const showModal = (data_images) => {
+   console.log('data_images', data_images);
+
+   // render image clicked
    const modal_body_img = document.querySelector('.modal-body');
-   let html_img = `<img src="${params}" alt="" />`;
+   let html_img = `<img src="${data_images.image}" alt="" />`;
    modal_body_img.innerHTML = html_img;
 
-   // user
+   //render user
    const contents_header = document.querySelector('.contents-header');
    let html_header = `                                 
     <div class="header-img">
-       <img src="../public/image/nguyen.png" alt="" />
+       <img src="${data_images.avatar}" alt="" />
     </div>
     <div class="header-name d-flex flex-column">
-       <span>Tuan le</span>
-       <span class="fst-italic fw-lighter">@tuanle</span>
+       <span>${data_images.user}</span>
+       <span class="fst-italic fw-lighter">${data_images.id_user}</span>
     </div>
     <div class="header-setting">
        <i class="fa-solid fa-grip"></i>
     </div>`;
    contents_header.innerHTML = html_header;
-   // content
+
+   //render content
    const contents_body = document.querySelector('.contents-body');
-   let html_body = `                                 
-    <p>
-       jksdfhjskdfhjsdkmncbfx sdljfhlds sdfjsijdlkjf slkdjf sdk
-    </p>`;
+   let html_body = `<p>${data_images.contents}</p>`;
+
    contents_body.innerHTML = html_body;
-   // comment
+
+   //render comment
    const comments_list = document.querySelector('.comments-list');
-   const html_comments = data_comment.map((item) => {
+   const html_comments = data_images.data.map((item) => {
       return `
        <div
           class="user-other d-flex flex-row justify-content-between gap-2 mb-3">
           <div class="img-user">
-             <img src="${item.image}" alt="" />
+             <img src="${item.avatar}" alt="" />
           </div>
           <div
              class="user-comment d-flex flex-column justify-content-between">
