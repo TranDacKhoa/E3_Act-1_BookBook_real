aa30460bc89bce226ed5ca50e8be77cdcb2be339
tathankochi <75437017+tathankochi@users.noreply.github.com>
2022-12-27 17:38:05 +0700

    Add files via upload

--- js/report/modalrp.js
+++ js/report/modalrp.js
@@ -5,18 +5,18 @@ const header_user = document.querySelector(".header-name");
 const contents_body = document.querySelector(".contents-body");
 const contents_comments = document.querySelector(".contents-comments");
 export const showModal = (data_images) => {
-  //image
-  const html_image = `<img src="${data_images.image}" alt="" />`;
-  // user
-  const html_avatar = `<img src="${data_images.avatar}" alt="" />`;
-  const html_user = `                                 
+   //image
+   const html_image = `<img src="${data_images.image}" alt="" />`;
+   // user
+   const html_avatar = `<img src="${data_images.avatar}" alt="" />`;
+   const html_user = `                                 
    <span><b>${data_images.user}</b></span>
    <span class="fst-italic fw-lighter">${data_images.id_user}</span>`;
-  // content
-  const html_contents = `<p>${data_images.contents}</p>`;
-  // comments
-  const html_comments = data_images.data.map((item) => {
-    return `
+   // content
+   const html_contents = `<p>${data_images.contents}</p>`;
+   // comments
+   const html_comments = data_images.data.map((item) => {
+      return `
       <div
          class="user-other d-flex flex-row justify-content-between gap-2 mb-3">
          <div class="img-user">
@@ -30,16 +30,12 @@ export const showModal = (data_images) => {
             </div>
          </div>
       </div>`;
-  });
+   });
 
-  //
-  modal_body.innerHTML = html_image;
-  header_avatar.innerHTML = html_avatar;
-  header_user.innerHTML = html_user;
-  contents_body.innerHTML = html_contents;
-  contents_comments.innerHTML = html_comments.join("");
+   //
+   modal_body.innerHTML = html_image;
+   header_avatar.innerHTML = html_avatar;
+   header_user.innerHTML = html_user;
+   contents_body.innerHTML = html_contents;
+   contents_comments.innerHTML = html_comments.join("");
 };
-const btn_show_message = document.querySelector(".btn-delete");
-btn_show_message.addEventListener("click", () => {
-  document.querySelector(".modal-delete").style.display = "block";
-});
