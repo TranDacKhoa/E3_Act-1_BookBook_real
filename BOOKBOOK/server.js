dde3ed9cd6d308cf39ef977d59fb5b9b6a8859a8
20120206 <20120206@student.hcmus.edu.vn>
2022-12-27 18:51:59 +0700

    remove comment sync in server.js

--- BOOKBOOK/server.js
+++ BOOKBOOK/server.js
@@ -5,13 +5,9 @@ const sequelize = require("./database/db").sequelize;
 const port = config.PORT || 3000;
 
 // sync model and db before start server
-// sequelize.sync({ alter: true }).then(() => {
-//   console.log("\nSync database\n");
-//   return app.listen(port, async () => {
-//     console.log(`Server is running on port ${port}\n`);
-//   });
-// });
-
-return app.listen(port, async () => {
-  console.log(`Server is running on port ${port}\n`);
+sequelize.sync({ alter: true }).then(() => {
+  console.log("\nSync database\n");
+  return app.listen(port, async () => {
+    console.log(`Server is running on port ${port}\n`);
+  });
 });
