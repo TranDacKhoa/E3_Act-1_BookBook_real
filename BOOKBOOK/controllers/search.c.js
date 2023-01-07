7af941d8fa11e57f4d1289ec7ad88238e99e591c
20120206 <nguyenngocthuy9a1@gmail.com>
2023-01-07 22:04:05 +0700

    nothing

--- BOOKBOOK/controllers/search.c.js
+++ BOOKBOOK/controllers/search.c.js
@@ -1,14 +1,17 @@
 const searchS = require("../services/searchServices");
 const hbsHelpers = require("../helpers/hbs_helpers.js");
+const userS = require("../services/userServices");
 module.exports.searchPeople = async (req, res, next) => {
   var input = req.query.user;
   if (input === undefined || input.trim() === "") {
     res.redirect("/");
   } else {
     const result = await searchS.searchUser(input);
+    // const following = await userS.getFollowingList(req.user.username)
     res.render("search", {
       result: result,
       query: input,
+      // following: following,
       helpers: hbsHelpers,
       user: {
         avatar: req.user.profile.avatar,
