70f7bfa674e322e14a940f5a50fc35f3ddf006f3
20120540 <st3phencs@gmail.com>
2023-01-11 10:16:26 +0700

    fix maybe you know

--- BOOKBOOK/configs/config.js
+++ BOOKBOOK/configs/config.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv").config({ path: __dirname + "/.env" });
 module.exports = {
   //muốn chạy db local để code thì chọn NODE_ENV
   //muốn chạy db on host để test thì chọn NODE_ENV1
-  //NODE_ENV: process.env.NODE_ENV,
+  // NODE_ENV: process.env.NODE_ENV,
   NODE_ENV: process.env.NODE_ENV1,
   PORT: process.env.PORT,
   HOST: process.env.HOST,
@@ -12,8 +12,8 @@ module.exports = {
   DB_DIALECT: process.env.DB_DIALECT,
   DB_HOST: process.env.DB_HOST,
   DB_USERNAME: process.env.DB_USERNAME,
-  // DB_PWD: process.env.DB_PWD,
-  DB_PWD: process.env.DB_PWD1,
+  DB_PWD: process.env.DB_PWD,
+  // DB_PWD: process.env.DB_PWD1,
   // DB_PWD: process.env.DB_PWD2,
 
   SESSION_SECRETKEY: process.env.SESSION_SECRETKEY,
